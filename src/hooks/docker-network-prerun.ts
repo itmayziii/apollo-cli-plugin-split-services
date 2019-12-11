6af34fdc0e9e4dc65cb7d8101ec542d3c03c917b
itmayziii <tommymay37@gmail.com>
2019-12-11 08:24:27 -0500

    fix(docker-network-prerun): now try/catching an error that gets thrown when not config exists

--- src/hooks/docker-network-prerun.ts
+++ src/hooks/docker-network-prerun.ts
@@ -3,10 +3,18 @@ import Docker from 'dockerode'
 import { dockerNetwork } from '../hook-fns/docker-network'
 import { getGatewayApolloConfig } from '../helpers'
 import * as parser from '@oclif/parser'
+import { ApolloConfig, GatewayConfig } from '../interfaces/apollo-config'
 
 const dockerNetworkPrerun: Hook<'prerun'> = function dockerNetworkPrerunHook (options) {
   const parsedOutput = parser.parse(options.argv, options.Command)
-  const apolloConfig = getGatewayApolloConfig(parsedOutput.flags.config || 'apollo.config.js')
+  let apolloConfig: ApolloConfig<GatewayConfig>
+  try {
+    apolloConfig = getGatewayApolloConfig(parsedOutput.flags.config || 'apollo.config.js')
+  } catch (e) {
+    // We really don't care do anything if there is no apollo config.
+    return
+  }
+
   return dockerNetwork(options, apolloConfig, new Docker())
 }
 
